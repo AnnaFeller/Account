9bd5667c01db349256f7bfa39c494341e6ac8cc8
Anna_F <felleranna31@gmail.com>
2025-08-26 11:32:00 +0300

    homework

--- src/features/api/accountApi.ts
+++ src/features/api/accountApi.ts
@@ -1,6 +1,6 @@
 import {createAsyncThunk} from "@reduxjs/toolkit";
-import type {UserData, userRegister} from "../../utils/types";
-import {base_url, base_user, createToken, createUser} from "../../utils/constatnts.ts";
+import type { UserLogin, userRegister} from "../../utils/types";
+import {base_url, base_user, createToken} from "../../utils/constatnts.ts";
 
 export const registerUser = createAsyncThunk(
     'user/register',
@@ -26,22 +26,23 @@ export const registerUser = createAsyncThunk(
 
 export const fetchUser = createAsyncThunk(
     'user/fetch',
-    async (user:  UserData) => {
-const response = await fetch(`${base_user}/account/fetch`, {
-    method: "POST",
-    headers: {
-        "Authorization": createToken(user.firstName, user.lastName)
-    },
-    body: JSON.stringify(user)
-})
+    async (user: UserLogin) => {
+        const response = await fetch(`${base_user}/fetch`, {
+            method: "POST",
+            headers: {
+                "Authorization": createToken(user.login, user.password),
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(user)
+        })
         if (response.status === 401) {
-            throw new Error(`UUser ${user.firstName} ,${user.lastName}already Unauthorized`);
+            throw new Error(`User ${user.login}already Unauthorized`);
         }
         if (!response.ok) {
             throw new Error(`Something went wrong`);
         }
         const data = await response.json();
-        const token = createUser(user.firstName, user.lastName);
+        const token = createToken(user.login, user.password);
         return {user: data, token};
     }
 )
--- src/utils/types.d.ts
+++ src/utils/types.d.ts
@@ -15,3 +15,8 @@ export interface userRegister extends UserData  {
     login:string;
     password: string;
 }
+
+export interface UserLogin {
+    login: string;
+    password: string;
+}
